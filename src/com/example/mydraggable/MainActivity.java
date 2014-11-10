af55332d3d10b73b564e5f037f5847c34c4593a9
kobake <kobake@users.sourceforge.net>
2014-11-11 03:29:31 +0900

    Moving object place when drag and drop

--- src/com/example/mydraggable/MainActivity.java
+++ src/com/example/mydraggable/MainActivity.java
@@ -1,16 +1,92 @@
 package com.example.mydraggable;
 
+import android.R.integer;
 import android.app.Activity;
+import android.content.ClipData;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnLongClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	TextView[] mTextViews = new TextView[4];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// TextView取得
+		int[] ids = new int[]{ R.id.textView1, R.id.textView2, R.id.textView3, R.id.textView4 };
+		for(int i = 0; i < 4; i++){
+			mTextViews[i] = (TextView)findViewById(ids[i]);
+		}
+
+		// 各種設定
+		for(int i = 0; i < 4; i++){
+			// ロングクリック検出・ドラッグ開始
+			mTextViews[i].setOnLongClickListener(new OnLongClickListener() {
+				@Override
+				public boolean onLongClick(View v) {
+					// データ準備
+					TextView tv = (TextView)v;
+					ClipData data = ClipData.newPlainText("test", "drag:" + tv.getText().toString());
+					// ドラッグ開始
+					v.startDrag(data, new DragShadowBuilder(v), v, 0);
+					return true;
+				}
+			});
+			// ドロップ受付
+			mTextViews[i].setOnDragListener(new OnDragListener() {
+				@Override
+				public boolean onDrag(View v, DragEvent event) {
+					TextView from = (TextView)event.getLocalState();
+					TextView to = (TextView)v;
+					String pos = String.format("%s -> %s [%.2f, %.2f] ",
+						from.getText().toString(), to.getText().toString(), event.getX(), event.getY());
+					switch(event.getAction()){
+					case DragEvent.ACTION_DRAG_STARTED:
+						// 自分自身へはドロップしない
+						if(from == to){
+							return false;
+						}
+						Log.d("test", pos + "drag started.");
+						return true;
+					case DragEvent.ACTION_DRAG_ENDED:
+						Log.d("test", pos + "drag ended.");
+						return true;
+					case DragEvent.ACTION_DRAG_LOCATION:
+						// Log.d("test", pos + "drag location."); // 量が多いのでログは出さない
+						return true;
+					case DragEvent.ACTION_DROP:
+						// ログ
+						Log.d("test", pos + "drop.");
+						// fromをtoの直前に移動する
+						{
+							ViewGroup parent = (ViewGroup)from.getParent();
+							parent.removeView(from);
+							parent.addView(from, parent.indexOfChild(to));
+						}
+						return true;
+					case DragEvent.ACTION_DRAG_ENTERED:
+						Log.d("test", pos + "drag entered.");
+						return true;
+					case DragEvent.ACTION_DRAG_EXITED:
+						Log.d("test", pos + "drag exited.");
+						return true;
+					}
+					// TODO Auto-generated method stub
+					return false;
+				}
+			});
+		}
 	}
 
 	@Override
